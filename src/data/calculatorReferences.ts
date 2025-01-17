ca31513165b2ae0e4dac90f647c927203ebceb7e
hessuew <juhanijuusola@gmail.com>
2025-01-17 04:55:38 +0200

    feat: waist height calculator references, ItemGridLink flex col

---
+++ src/data/calculatorReferences.ts
@@ -0,0 +1,53 @@
+interface Reference {
+  title: string;
+  subtitle: string;
+  description: string;
+  url?: string;
+}
+
+export const references: Reference[] = [
+  {
+    title: 'The Lancet Diabetes & Endocrinology',
+    subtitle: 'Definition and diagnostic criteria of clinical obesity',
+    description: 'Rubino F, Cummings DE, Eckel RH, et al. The Lancet Diabetes & Endocrinology 2025.',
+    url: 'https://doi.org/10.1016/S2213-8587(24)00316-4',
+  },
+  {
+    title: 'BMC Medicine Research',
+    subtitle: 'Establishing international optimal cut-offs of waist-to-height ratio for predicting cardiometabolic risk in children and adolescents aged 6-18 years',
+    description: 'Zong X, Kelishadi R, Hong YM, et al. BMC Med. 2023 Nov 15;21(1):442.',
+    url: 'https://doi.org/10.1186/s12916-023-03169-y',
+  },
+  {
+    title: 'Research Article in Pediatric Research',
+    subtitle:
+      'Waist-circumference-to-height-ratio had better longitudinal agreement with DEXA-measured fat mass than BMI in 7237 children',
+    description: 'Agbaje AO. Pediatr Res. 2024 Oct;96(5)5:1369-1380.',
+    url: 'https://doi.org/10.1038/s41390-024-03112-8',
+  },
+  {
+    title: 'Nature Medicine Commentary',
+    subtitle: 'Time for a new framework that treats obesity in children as an adiposity-based chronic disease',
+    description: "Manco M, Helgason T, Körner A, Nowicka P, O'Malley G, Baker JL. Nat Med. 2024 Dec;30(12):3396.",
+    url: 'https://doi.org/10.1038/s41591-024-03292-0',
+  },
+  {
+    title: 'Journal of the American College of Cardiology',
+    subtitle: 'Adiposopathy is "sick fat" a cardiovascular disease?',
+    description: 'Bays HE. J Am Coll Cardiol. 2011 Jun 21;57(25):2461-73.',
+    url: 'https://doi.org/10.1016/j.jacc.2011.02.038',
+  },
+  {
+    title: 'European Association for the Study of Obesity Interview',
+    subtitle: 'Changing the way we measure childhood obesity',
+    description:
+      'Interview with Andrew Agbaje discussing the implications of the research findings for childhood obesity measurement.',
+    url: 'https://easo.org/changing-the-way-we-measure-childhood-obesity-in-conversation-with-andrew-agbaje/',
+  },
+  {
+    title: 'BBC World News Interview',
+    subtitle: 'Live TV Interview',
+    description: 'Discussion on the importance of waist-to-height ratio in measuring childhood obesity.',
+    url: 'https://www.youtube.com/watch?v=OiA9HlVHsuw',
+  },
+];
